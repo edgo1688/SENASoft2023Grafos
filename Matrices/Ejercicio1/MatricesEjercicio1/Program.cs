f2b9372411badfda98dc32ac539df247e92f702e
Edwin Fermin Gomez Quiñonez <edgo1688@gmail.com>
2023-09-20 12:56:55 -0500

    Matrices second exercise (min y max values)

--- Matrices/Ejercicio1/MatricesEjercicio1/Program.cs
+++ Matrices/Ejercicio1/MatricesEjercicio1/Program.cs
@@ -23,6 +23,38 @@ namespace Matrices
             int[,] matrizAleatoria = inicializarMatriz(5, 4, 300);
             imprimirMatriz(matrizAleatoria);
 
+            //encontrar el menor y el mayor valor, e imprimirlos con sus posiciones
+            int i_min = 0, j_min = 0;
+            int i_max = 0, j_max = 0;
+
+            int min = matrizAleatoria[i_min, j_min];
+            int max = matrizAleatoria[i_max, j_max];
+
+            //ciclos anidados
+            for (int i = 0; i < matrizAleatoria.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizAleatoria.GetLength(1); j++)
+                {
+                    if(matrizAleatoria[i, j] < min)
+                    {
+                        min = matrizAleatoria[i,j];
+                        i_min = i;
+                        j_min = j;
+                    }
+                    
+                    if(matrizAleatoria[i, j] > max)
+                    {
+                        max = matrizAleatoria[i,j];
+                        i_max = i;
+                        j_max = j;
+                    }
+                }
+            }
+
+            //Console.WriteLine("El valor mínimo es: " + min+ ", y se encuentra en la posición " + i_min);
+            Console.WriteLine($"El valor mínimo es: {min} y se encuentra en la posición [{i_min},{j_min}]");
+            Console.WriteLine($"El valor máximo es: {max} y se encuentra en la posición [{i_max},{j_max}]");
+
         }
 
         //función imprimir
